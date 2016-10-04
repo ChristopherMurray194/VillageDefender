8e676d6cb3f66db2e6df3efa0c135d33f353d835
Christopher Murray <chris.murray194@hotmail.co.uk>
2016-10-04 16:24:47 +0100

    Enemy animation controller and EnemyMovement script added.

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float movementSpeed = 3f;  // Movement speed of skeleton
+    Transform player;   // Position of the player
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    NavMeshAgent nav;   // Reference to the NavMeshAgent
+
+    void Awake()
+    {
+        // Find the player's position
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+        // Set the initial movement speed
+        nav.speed = movementSpeed;
+    }
+
+    void Update()
+    {
+        // if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        //{
+            // Move to the player's position
+            nav.SetDestination(player.position);
+        //}
+        //else
+        //    nav.enabled = false;
+    }
+}
