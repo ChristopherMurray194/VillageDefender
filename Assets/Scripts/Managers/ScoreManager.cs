587f6f058988a2ab8f204b33dc48fb82e2e3c8f0
Christopher Murray <chris.murray194@hotmail.co.uk>
2016-10-06 04:15:44 +0100

    ScoreManager script added.

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+    Text text;
+
+    void Awake()
+    {
+        // Reset the score if we die. As the score variable is static,
+        // when the game is reset the value will not change unless explicitely changed.
+        score = 0;
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = "Score: " + score;
+    }
+}
