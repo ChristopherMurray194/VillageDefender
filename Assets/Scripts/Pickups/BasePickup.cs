ede0428e91ab78ad71281ce9f0392121753eb029
Christopher Murray <chris.murray194@hotmail.co.uk>
2016-10-12 20:41:19 +0100

    Script added for base pickup behaviour, rotates on the Y axis.

---
+++ Assets/Scripts/Pickups/BasePickup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePickup : MonoBehaviour
+{
+    void Update()
+    {
+        // Rotate the pickup 90 degrees in the Y axis
+        transform.Rotate(new Vector3(0f, 90f, 0f) * Time.deltaTime, Space.World);
+    }
+}
