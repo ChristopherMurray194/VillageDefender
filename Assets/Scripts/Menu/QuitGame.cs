e21742a00144fe5c2d60824eeefa462005ef0270
Christopher Murray <chris.murray194@hotmail.co.uk>
2016-10-17 22:06:24 +0100

    Quit functionality added.

---
+++ Assets/Scripts/Menu/QuitGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        // If the game is running in the editor
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else // Otherwise when running in a build
+                Application.Quit();
+        #endif
+    }
+}
