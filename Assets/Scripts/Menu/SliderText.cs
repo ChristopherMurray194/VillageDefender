219f5b54648a0ef38d1ddae94dbe12827b895896
Christopher Murray <chris.murray194@hotmail.co.uk>
2016-10-18 02:52:49 +0100

    Music volume slider affects music volume, and value displayed as text.

---
+++ Assets/Scripts/Menu/SliderText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderText : MonoBehaviour
+{
+    Text text;
+    Slider slider;
+    float percentage = 0f;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        slider = GetComponentInParent<Slider>();
+        percentage = slider.value * 100f;
+        text.text = ((int)percentage).ToString() + "%";
+    }
+
+    void Update()
+    {
+        percentage = slider.value * 100f;
+        text.text = ((int)percentage).ToString() + "%";
+    }
+}
